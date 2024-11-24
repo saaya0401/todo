4609377eb101c6992ac5a070dc64414c352dd298
koba saaya <saaya.koba0401@gmail.com>
2024-11-24 16:52:27 +0900

    追加機能実装

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -6,18 +6,27 @@
 
 @section('content')
 <div class="alert">
-
+    @if(session('message'))
     <div class="alert__success">
         カテゴリを作成しました
     </div>
-
+    @endif
+    @if($errors->any())
+    <div class="alert__danger">
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 </div>
 
 <div class="category-content">
-    <form class="category-create" action="" method="post">
+    <form class="category-create" action="/categories" method="post">
         @csrf
         <div class="category-create__text">
-            <input class="category-create__input" type="text" name="name" value="">
+            <input class="category-create__input" type="text" name="name" value="{{old('name')}}">
         </div>
         <div class="category-create__button">
             <button class="category-create__button-submit" type="submit">作成</button>
@@ -27,14 +36,14 @@
         <tr class="category-table__row">
             <th class="category-table__header">category</th>
         </tr>
-
+        @foreach($categories as $category)
         <tr class="category-table__row">
             <td class="category-table__update">
                 <form class="update-form" action="" method="post">
                     @csrf
                     <div class="update-form__content">
-                        <input class="update-form__input" type="text" name="name" value="text">
-                        <input type="hidden" name="id"value="">
+                        <input class="update-form__input" type="text" name="name" value="{{$category['name']}}">
+                        <input type="hidden" name="id"value="{{$category['id']}}">
                     </div>
                     <div class="update-form__button">
                         <button class="update-form__button-submit" type="submit">更新</button>
@@ -45,12 +54,13 @@
                 <form class="delete-form" action="" method="post">
                     @csrf
                     <div class="delete-form__button">
-                        <input type="hidden" name="id" value="">
+                        <input type="hidden" name="id" value="{{$category['id']}}">
                         <button class="delete-form__button-submit">削除</button>
                     </div>
                 </form>
             </td>
         </tr>
+        @endforeach
     </table>
 </div>
 @endsection
